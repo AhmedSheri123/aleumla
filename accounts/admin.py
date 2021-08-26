5fa4c2f118ebe2513395a9072c4a6708dc92f23e
AhmedSheri123 <85445168+AhmedSheri123@users.noreply.github.com>
2021-08-26 22:25:12 +0300

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from accounts.forms import resetpassword
-from django.contrib import admin
-from .models import UserProfile, Confirm_Account, Reset_Password
-
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(Confirm_Account)
-admin.site.register(Reset_Password)
