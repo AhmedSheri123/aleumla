9766bd8ec23d405bc769b44b5e06ea87a33d953a
AhmedSheri123 <85445168+AhmedSheri123@users.noreply.github.com>
2021-08-28 04:26:26 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*=h#!v0ywa+3=mph2f_7f1o#7ybxp)=io2947fez26b2(umw)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['aleumla.herokuapp.com', '127.0.0.1']
 
